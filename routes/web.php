bd2c06739442024102e186383149348e08b2b7ac
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-08 21:38:01 +0200

    change route service provider home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    // Route::resource('projects', ProjectController::class)->parameters([
+    //     'projects' => 'project:slug'
+    // ]);
+    // Route::resource('types', TypeController::class)->parameters([
+    //     'types' => 'type:slug'
+    // ]);
+    // Route::resource('technologies', TechnologyController::class)->parameters([
+    //     'technologies' => 'technology:slug'
+    // ]);
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
