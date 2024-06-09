c31501e3901350ddb63e05c5747ee6c0dfc6017f
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-09 16:08:29 +0200

    fix delete modal style

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -26,7 +26,7 @@
 
 
                             {{-- delete modal --}}
-                            <a href="#" data-modal-target="modal-{{ $project->id }}"
+                            <a data-modal-target="modal-{{ $project->id }}"
                                 class="text-white bg-gray-700 hover:bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Delete</a>
 
                             <x-delete-modal :project="$project" />
