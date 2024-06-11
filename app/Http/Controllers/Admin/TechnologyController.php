d5b936e85f313360977c7d480fba040d3b6a3730
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-11 19:18:08 +0200

    remove unnecessary methods from TechnologyController

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -20,14 +20,6 @@ class TechnologyController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return back();
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTechnologyRequest $request)
@@ -37,22 +29,6 @@ class TechnologyController extends Controller
 
         $technology = Technology::create($validated);
         return to_route('admin.technologies.index')->with('message', "Type $technology->name created successfully");
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Technology $technology)
-    {
-        return back();
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Technology $technology)
-    {
-        return back();
     }
 
     /**
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('admin.dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img width="30" style="margin-left: 10px" src="{{ asset('logo-backend.png') }}" alt="fwef">
+                        {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> --}}
                     </a>
                 </div>
 
