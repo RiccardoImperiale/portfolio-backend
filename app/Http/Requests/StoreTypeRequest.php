4559f6531bf44039faaa929512310c66b4c7bae7
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-14 17:11:20 +0200

    fix store type request

--- app/Http/Requests/StoreTypeRequest.php
+++ app/Http/Requests/StoreTypeRequest.php
@@ -22,7 +22,7 @@ class StoreTypeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|unique:types,name|min:3|max:25'
+            'name' => 'required|string|unique:types,name|min:3|max:25'
         ];
     }
 }
